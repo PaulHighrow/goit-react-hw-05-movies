1ddf74e20a224d23a0bb343f8b5b6aad45dcae69
PaulHighrow <106202828+PaulHighrow@users.noreply.github.com>
2023-02-11 21:22:18 +0000

    Deploying to gh-pages from @ PaulHighrow/goit-react-hw-05-movies@fb7565caf0010ed7b19ac7a004714b432130cf6b 🚀

--- static/js/Reviews.941301b2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[753],{186:function(e,t,i){i.r(t);var s=i(184);t.default=function(){return(0,s.jsx)("h1",{children:"Reviews"})}}}]);
-//# sourceMappingURL=Reviews.941301b2.chunk.js.map
